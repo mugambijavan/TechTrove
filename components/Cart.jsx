5152965839b779763bcfb7f7a8b6116e5328342d
Mugambi Javan <mugambijavan@gmail.com>
2023-06-23 14:53:30 +0300

    name changes

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -47,7 +47,7 @@ const Cart = () => {
         {cartItems.length < 1 && (
           <div className="empty-cart">
             <AiOutlineShopping size={150} />
-            <h3>Your shopping bag is empty</h3>
+            <h3>Your cart is currently empty.</h3>
             <Link href="/">
               <button
                 type="button"
