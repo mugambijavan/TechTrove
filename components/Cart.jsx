c4d791420c09b1f11e9e3bb2a059a41d37b2208a
Mugambi Javan <mugambijavan@gmail.com>
2023-07-03 23:58:24 +0300

    TechTrove ready to deploy

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -47,7 +47,7 @@ const Cart = () => {
         {cartItems.length < 1 && (
           <div className="empty-cart">
             <AiOutlineShopping size={150} />
-            <h3>Your cart is currently empty.</h3>
+            <h3>Your shopping bag is empty</h3>
             <Link href="/">
               <button
                 type="button"
@@ -67,7 +67,7 @@ const Cart = () => {
               <div className="item-desc">
                 <div className="flex top">
                   <h5>{item.name}</h5>
-                  <h4>${item.price}</h4>
+                  <h4>Ksh{item.price}</h4>
                 </div>
                 <div className="flex bottom">
                   <div>
@@ -95,7 +95,7 @@ const Cart = () => {
           <div className="cart-bottom">
             <div className="total">
               <h3>Subtotal:</h3>
-              <h3>${totalPrice}</h3>
+              <h3>Ksh{totalPrice}</h3>
             </div>
             <div className="btn-container">
               <button type="button" className="btn" onClick={handleCheckout}>
