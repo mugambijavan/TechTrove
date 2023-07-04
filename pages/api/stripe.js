921c50558b4a6c20a9de00226b1d1b74c28ea9f4
Mugambi Javan <mugambijavan@gmail.com>
2023-07-04 23:39:30 +0300

    stripe init

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -1,6 +1,6 @@
 import Stripe from 'stripe';
 
-const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
@@ -11,8 +11,9 @@ export default async function handler(req, res) {
         payment_method_types: ['card'],
         billing_address_collection: 'auto',
         shipping_options: [
-          { shipping_rate: 'shr_1Kn3IaEnylLNWUqj5rqhg9oV' },
-        ],
+          { shipping_rate: 'shr_1NQDIWFis3lggVOm9oAfwM9E' },
+          { shipping_rate: 'shr_1NQDLUFis3lggVOmCdZixRPm' },
+        ], 
         line_items: req.body.map((item) => {
           const img = item.image[0].asset._ref;
           const newImage = img.replace('image-', 'https://cdn.sanity.io/images/vfxfwnaw/production/').replace('-webp', '.webp');
